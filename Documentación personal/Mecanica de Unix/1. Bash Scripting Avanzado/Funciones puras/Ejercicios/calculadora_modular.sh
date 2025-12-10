74c47d95ff962c61fe21b994a41a88dec196332b
Lil_Carpi <carpi@mailcarpi.xyz>
2025-12-10 19:54:19 +0100

    vault backup: 2025-12-10 19:54:19

--- Documentación personal/Mecanica de Unix/1. Bash Scripting Avanzado/Funciones puras/Ejercicios/calculadora_modular.sh
+++ Documentación personal/Mecanica de Unix/1. Bash Scripting Avanzado/Funciones puras/Ejercicios/calculadora_modular.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
+
+echo "Opciones posibles:"
+echo "1. Sumar"
+echo "2. Restar"
+echo "3. Multiplicacion"
+echo "4. Division"
+read input
+case $input in
+    1) $suma;;
+    2) $resta;;
+    3) $multiplicacion;;
+    4) $division;;
+esac
+
+
 suma() {
     local num1=$1
     local num2=$2
-    local resultado=$(( $num1 + $num2))
     echo $(( $1 + $2))
 }
 
 resta() {
     local num1=$1
     local num2=$2
-    local resultado=$(( $num1 - $num2))
     echo $(( $1 - $2))
 }
 
 multiplicacion() {
     local num1=$1
     local num2=$2
-    local resultado=$(( $num1 * $num2))
     echo $(( $1 * $2))
 }
 
@@ -27,6 +39,6 @@ division() {
         echo "ERROR_DIV_0"
         return 1
     fi
-    
     echo $(( $1 / $2))
 }
+
